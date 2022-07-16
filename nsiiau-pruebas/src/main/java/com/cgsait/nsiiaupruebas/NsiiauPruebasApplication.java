f1d89676d1752b7005955292e1ee1bc9ddc391bf
Noé García <nooe.mtz@hotmail.com>
2022-07-16 16:40:30 -0500

    proyecto vacio

---
+++ nsiiau-pruebas/src/main/java/com/cgsait/nsiiaupruebas/NsiiauPruebasApplication.java
@@ -0,0 +1,13 @@
+package com.cgsait.nsiiaupruebas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NsiiauPruebasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NsiiauPruebasApplication.class, args);
+	}
+
+}
---
+++ nsiiau-pruebas/src/test/java/com/cgsait/nsiiaupruebas/NsiiauPruebasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgsait.nsiiaupruebas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NsiiauPruebasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
